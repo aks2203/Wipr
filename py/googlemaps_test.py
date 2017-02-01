cab070b02989c526259d89368c1b8a7095b46998
Avi <aks2203@columbia.edu>
2017-01-31 20:55:58 -0500

    googlemaps_test.py in progress.

--- py/googlemaps_test.py
+++ py/googlemaps_test.py
@@ -5,4 +5,18 @@
 ###################################
 
 # Google Maps Api testing 
-import googlemaps
+from googlemaps import Client
+
+# api_key = 'AIzaSyDJJGsUGU96GMLYUdeOdufkoIG-x2Soo9s'
+api_key = 'AIzaSyDg-yjmIEndzcGRaHxH8-Jx2UNAerk7430'
+gmaps = Client(api_key)
+
+address = '431 Riverside Drive, New York, NY 10027'
+lat, lng = gmaps.geocode(address)[0]['geometry']['location'].values()
+print lat, lng
+
+# dest = gmaps.reverse_geocode([lat, lng])[0]['geometry']['formatted_address'].values()
+# print dest
+
+one = gmaps.reverse_geocode([lat, lng])[0].keys()
+print one
