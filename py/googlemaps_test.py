1fc6f9bb43000256dfa1a2c68d5825cfbf6eed42
Avi <aks2203@columbia.edu>
2017-02-01 11:32:39 -0500

    route as list of lat,lng working.

--- py/googlemaps_test.py
+++ py/googlemaps_test.py
@@ -6,17 +6,32 @@
 
 # Google Maps Api testing 
 from googlemaps import Client
+import gmplot
 
-# api_key = 'AIzaSyDJJGsUGU96GMLYUdeOdufkoIG-x2Soo9s'
-api_key = 'AIzaSyDg-yjmIEndzcGRaHxH8-Jx2UNAerk7430'
-gmaps = Client(api_key)
+api_key_route = 'AIzaSyDJJGsUGU96GMLYUdeOdufkoIG-x2Soo9s'
+
+api_key_maps = 'AIzaSyDg-yjmIEndzcGRaHxH8-Jx2UNAerk7430'
+gmaps = Client(api_key_maps)
+
 
 address = '431 Riverside Drive, New York, NY 10027'
 lat, lng = gmaps.geocode(address)[0]['geometry']['location'].values()
 print lat, lng
 
-# dest = gmaps.reverse_geocode([lat, lng])[0]['geometry']['formatted_address'].values()
-# print dest
+route_map = Client(api_key_route)
+routes = route_map.directions('330 West 72, New York 10023', '3198 Old Post Drive, Baltimore, 21208')
 
-one = gmaps.reverse_geocode([lat, lng])[0].keys()
-print one
+
+lats = []
+longs = []
+
+for i, step in enumerate(routes[0]['legs'][0]['steps']):
+    lats.append(step['start_location']['lat'])
+    longs.append(step['start_location']['lng'])
+    lats.append(step['end_location']['lat'])
+    longs.append(step['end_location']['lng'])
+
+gmap_plt = gmplot.GoogleMapPlotter(lat, lng, 16)
+
+gmap_plt.plot(lats, longs)
+gmap_plt.draw("mymap.html")
