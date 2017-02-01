041bf378e5da708cc05959d960b768c86682e2dc
Avi <aks2203@columbia.edu>
2017-02-01 11:39:17 -0500

    user.py added to plot route with user input origin and destination.

---
+++ py/user.py
@@ -0,0 +1,42 @@
+###################################
+#
+# Avi Schwarzschild
+#
+###################################
+
+# Google Maps Api user input
+
+from googlemaps import Client
+import gmplot
+
+api_key_route = 'AIzaSyDJJGsUGU96GMLYUdeOdufkoIG-x2Soo9s'
+api_key_maps = 'AIzaSyDg-yjmIEndzcGRaHxH8-Jx2UNAerk7430'
+gmaps = Client(api_key_maps)
+route_map = Client(api_key_route)
+
+
+origin = raw_input('Where are you starting? (please enter a valid address) ')
+dest = raw_input('Where are you going? (please enter a valid address) ')
+
+lat0, lng0 = gmaps.geocode(origin)[0]['geometry']['location'].values()
+print lat0, lng0
+
+lat1, lng1 = gmaps.geocode(dest)[0]['geometry']['location'].values()
+print lat1, lng1
+
+routes = route_map.directions(origin, dest)
+
+lats = []
+longs = []
+
+for i, step in enumerate(routes[0]['legs'][0]['steps']):
+    lats.append(step['start_location']['lat'])
+    longs.append(step['start_location']['lng'])
+    lats.append(step['end_location']['lat'])
+    longs.append(step['end_location']['lng'])
+
+
+gmap_plt = gmplot.GoogleMapPlotter((lat0 + lat1) / 2.0, (lng0 + lng1) / 2.0, 16)
+
+gmap_plt.plot(lats, longs)
+gmap_plt.draw("mymap.html")
